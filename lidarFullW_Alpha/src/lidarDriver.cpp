262ec37d92daff183be96ba74a0937c884b1ed5d
Ravi Shankar <ravi@lovelace.boisestate.edu>
2017-05-18 13:02:31 -0600

    Created: 	- lidarDriver.cpp	--> driver file 	- cmdLine.hpp		--> command line header file 	- cmdLine.cpp		--> contains the function to parse command line arguments

---
+++ lidarFullW_Alpha/src/lidarDriver.cpp
@@ -0,0 +1,15 @@
+/*
+ * File name: lidar_driver.cpp
+ * Created on: 17-May-2017
+ * Author: ravi
+ */
+#include <iostream>
+#include "cmdLine.hpp"
+
+using namespace std;
+
+int main (int argc,char *argv[])
+{
+parseCmdLineArgs(argc,argv);
+return 0;
+}

df7176cbf09423c63b965e3b40931dd66c70ad61
Ravi Shankar <ravi@lovelace.boisestate.edu>
2017-05-24 15:52:32 -0600

    Removed test cases to check where program crashes

--- lidarFullW_Alpha/src/testDriver.cpp
+++ lidarFullW_Alpha/src/testDriver.cpp
@@ -34,31 +34,8 @@ int testFileReader(){
   int passCount = 0;
   int failCount = 0;
 
-try{	
-    /* Test for a valid file name
-     * Expected result: Prints the first line of the file
-     */
-    parseFile("../README.md");
-    passCount++;
-  }catch(const std::exception& e){
-    failCount++;
-    std::cerr << "\nFAIL: Test 1 - Valid file" 
-              << std::endl;
-    std::cerr << e.what();
-  }
+  return failCount;
 
-try{	
-    /* Test for an invalid file name
-     * Expected result: Could not find the file
-     */
-    parseFile("test");
-    passCount++;
-  }catch(const std::exception& e){
-    failCount++;
-    std::cerr << "\nFAIL: Test 2 - Invalid file name" 
-              << std::endl;
-    std::cerr << e.what();
-  }
 }
 
 /**
@@ -114,124 +91,36 @@ int testCmdLine(){
      */
     noOfArgs = 2;
     strncpy( someArgs[1],"-f",2);
-    CmdLineArgs config = parseCmdLineArgs(noOfArgs,someArgs);
-    if(strncmp(config.inputFile(),"test",4)){
-      passCount++;
-    }else{
-        failCount++;
-        std::cerr << "\nFAIL: Test 2 - Valid option '-f' without argument" 
-                  << std::endl;
-    }
-  }catch(const std::exception& e){
-    failCount++;
-    std::cerr << "\nFAIL: Test 2 - Valid option '-f' without argument" 
-              << std::endl;
-    std::cerr << e.what();
-  }
-
-  try{	
-    /* Test for 2 command line arguments
-     * option: f
-     * argument: test
-     * Expected result: Print the first line of the 'test' file
-     */
-    noOfArgs = 3;
-    strncpy( someArgs[1],"-f",2);
-    strncpy( someArgs[2],"test",4);
-    parseCmdLineArgs(noOfArgs,someArgs);
-    CmdLineArgs config = parseCmdLineArgs(noOfArgs,someArgs);
-    if(strncmp(config.inputFile(),"test",4)){
-      passCount++;
-      
-    }else{
-        failCount++;
-        std::cerr << "\nFAIL: Test 3 - Valid option '-f' without argument" 
-                  << std::endl;
-    }
-  }catch(const std::exception& e){
-    failCount++;
-    std::cerr << "\nFAIL: Test 3 - Valid option '-f' without argument" 
-              << std::endl;
-    std::cerr << e.what();
-  }
-
-  try{	
-    /* Test for 2 command line arguments
-     * option: f
-     * argument: rest
-     * Expected result: Argument 'rest' is not a valid file
-     */
-    noOfArgs = 2;
-    strncpy( someArgs[1],"-f",2);
-    strncpy( someArgs[2],"rest",4);
     parseCmdLineArgs(noOfArgs,someArgs);
     passCount++;
-  }catch(const std::exception& e){
-    failCount++;
-    std::cout << "\nFAIL: Test 4 - Valid option '-f' without argument" 
-              << std::endl;
-    std::cerr << e.what();
+    }catch(const std::exception& e){
+      failCount++;
+      std::cerr << "FAIL: Test 2 - Valid option '-f' without argument" 
+                << std::endl;    
+      std::cerr << e.what();
   }
 
-  try{
-    /* Test for 2 command line arguments
-     * option: h
-     * argument: none
-     * Expected result: Prints usage
-     */
-    noOfArgs = 2;
-    strncpy( someArgs[1],"-g",2);
-    parseCmdLineArgs(noOfArgs,someArgs);try{	
+  try{	
     /* Test for 2 command line arguments
      * option: f
      * argument: none
      * Expected result: Indicate that an argument is required and print usage
      */
     noOfArgs = 2;
-    strncpy( someArgs[1],"-f",2);
-    CmdLineArgs config = parseCmdLineArgs(noOfArgs,someArgs);
-    if(strncmp(config.inputFile(),"test",4)){
-      passCount++;
-    }else{
-        failCount++;
-        std::cerr << "\nFAIL: Test 2 - Valid option '-f' without argument" 
-                  << std::endl;
-    }
-  }catch(const std::exception& e){
-    failCount++;
-    std::cerr << "\nFAIL: Test 2 - Valid option '-f' without argument" 
-              << std::endl;
-    std::cerr << e.what();
-  }
-    passCount++;
-  }catch(const std::exception& e){
-    failCount++;
-    std::cout << "\nFAIL: Test 5 - Valid option '-f' without argument" 
-              << std::endl;
-    std::cerr << e.what();
-  }
-
-  try{
-    /* Test for 2 command line arguments
-     * option: g
-     * argument: none
-     * Expected result: Indicate that the option is invalid and print usage
-     */
-    noOfArgs = 2;
     strncpy( someArgs[1],"-g",2);
     parseCmdLineArgs(noOfArgs,someArgs);
-    passCount++;
-  }catch(const std::exception& e){
     failCount++;
-    std::cout << "\nFAIL: Test 6 - Valid option '-f' without argument" 
-              << std::endl;
-    std::cerr << e.what();
+    }catch(const std::exception& e){
+      passCount++;
+      std::cerr << "FAIL: Test 3 - Invalid option '-f' without argument" 
+                << std::endl;    
+      std::cerr << e.what();
   }
 
 
   std::cerr << "\nTesting Complete: " << failCount << " of " <<
   failCount+passCount << " tests failed" << std::endl;
-	return failCount;
+  return failCount;
 }
 
 

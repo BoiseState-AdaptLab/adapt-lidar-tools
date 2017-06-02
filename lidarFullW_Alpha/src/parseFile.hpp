adf926bf8fafc522eee53782cbbd075bcc7550f4
Ravi Shankar <ravi@lovelace.boisestate.edu>
2017-06-02 11:11:01 -0600

    Moved gtests to deps

--- lidarFullW_Alpha/src/parseFile.hpp
+++ lidarFullW_Alpha/src/parseFile.hpp
@@ -7,6 +7,43 @@
 #ifndef PARSEFILE_HPP_
 #define PARSEFILE_HPP_
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class ParseFile{
+
+private:
+  //possible parameters
+
+
+public:
+  // help parameter
+  bool printFileMessage;
+
+    ParseFile();
+    void parse(int argc, char *argv[]);
+    std::string getUsageMessage();
+    void setUsageMessage();
+    void setInputFileName(char *args);
+    void setInputFileName(std::string filename);
+    std::string getInputFileName();
+
+  void parseFile(std::string filename);
+};
+
+
+
+#endif /* PARSEFILE_HPP_ */
+
+
+/* Original
+
+#ifndef PARSEFILE_HPP_
+#define PARSEFILE_HPP_
+
 void parseFile(char *fileName);
 
 #endif /* PARSEFILE_HPP_ */
+
+*/

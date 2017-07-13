a3bd150ca618fa3b5f1e9fc93cf24912e6edf046
Ravi Shankar <ravishankar@u.boisestate.edu>
2017-07-13 17:09:04 -0600

    checking id the wave is outgoing or returning

--- lidarFullW_Alpha/src/lidarDriver.cpp
+++ lidarFullW_Alpha/src/lidarDriver.cpp
@@ -56,6 +56,7 @@ int main (int argc, char *argv[]){
         
         gpsInfo.populateGPS(pReader);
         gpsInfo.displayGPSData();
+        
         if(pReader->read_waves()){
           for(int i = 0; i < pReader->waves->get_number_of_samplings(); i++){
             sampling = pReader->waves->get_sampling(i);
@@ -64,12 +65,21 @@ int main (int argc, char *argv[]){
               sampling->set_active_segment(j);
                 
                 for(int k = 0; k < maxCount; k++){
-                  
-                  if(k >= sampling->get_number_of_samples()){
-                      ad.data.push_back(0);
-                  } 
-                  else{
-                      ad.data.push_back(sampling->get_sample(k));
+                  if(sampling->get_type() == PULSEWAVES_OUTGOING){
+                    if(k >= sampling->get_number_of_samples()){
+                      ad.outgoingWave.push_back(0);
+                    } 
+                    else{
+                      ad.outgoingWave.push_back(sampling->get_sample(k));
+                    }
+                  }
+                  else if(sampling->get_type() == PULSEWAVES_RETURNING){
+                    if(k >= sampling->get_number_of_samples()){
+                      ad.returningWave.push_back(0);
+                    } 
+                    else{
+                      ad.returningWave.push_back(sampling->get_sample(k));
+                    }
                   }
                 }
             }

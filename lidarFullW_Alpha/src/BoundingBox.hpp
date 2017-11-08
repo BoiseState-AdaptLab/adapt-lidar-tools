bb0746883ed30cac424914f57305ba7abc21f80f
rvishnkr <ravishankar@u.boisestate.edu>
2017-11-08 11:57:32 -0700

    Bounding box

---
+++ lidarFullW_Alpha/src/BoundingBox.hpp
@@ -0,0 +1,18 @@
+#ifndef BOUNDINGBOX_HPP_
+#define BOUNDINGBOX_HPP_
+
+public:
+  //The min and max fields describing the boundary box that includes the first &
+  //last points of the sampled parts of the returning waveforms of all pulse
+  double bb_x_min;
+  double bb_y_min;
+  double bb_z_min;
+  double bb_x_max;
+  double bb_y_max;
+  double bb_z_max;
+
+  
+
+
+#endif /* BOUNDINGBOX_HPP_ */
+

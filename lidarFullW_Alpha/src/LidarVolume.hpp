8d69ca87d1f441d62b9b0cedd7ca30e1b7648084
rvishnkr <ravishankar@u.boisestate.edu>
2017-11-08 14:53:14 -0700

    added required headers

--- lidarFullW_Alpha/src/LidarVolume.hpp
+++ lidarFullW_Alpha/src/LidarVolume.hpp
@@ -7,34 +7,43 @@
 #ifndef LIDARVOLUME_HPP_
 #define LIDARVOLUME_HPP_
 
-public:
-  //The min and max fields describing the boundary box(bb) that includes the 
-  //first & last points of the sampled parts of the returning waveforms of 
-  //all pulses
-  double bb_x_min;
-  double bb_y_min;
-  double bb_z_min;
-  double bb_x_max;
-  double bb_y_max;
-  double bb_z_max;
+#include <iostream>
+#include "pulsereader.hpp"
+#include "pulsewriter.hpp"
 
-  //floor of the bb_mins
-  int i_min;
-  int j_min;
-  int k_min;
-  //ceil of the bb_maxes
-  int i_max;
-  int j_max;
-  int k_max;
+class LidarVolume{
 
-  //extent = i,j,k maxes - mins
-  int x_extent;
-  int y_extent;
-  int z_extent;
+  public:
+    //The min and max fields describing the boundary box(bb) that includes the 
+    //first & last points of the sampled parts of the returning waveforms of 
+    //all pulses
+    double bb_x_min;
+    double bb_y_min;
+    double bb_z_min;
+    double bb_x_max;
+    double bb_y_max;
+    double bb_z_max;
 
-  //Read and store the mins and maxes from the header, calculate and store the
-  //i, j,k values and the extents
-  CalculateBoundingBox(PULSEreader* pReader);
+    //floor of the bb_mins
+    int i_min;
+    int j_min;
+    int k_min;
+    //ceil of the bb_maxes
+    int i_max;
+    int j_max;
+    int k_max;
+
+    //extent = i,j,k maxes - mins
+    int x_extent;
+    int y_extent;
+    int z_extent;
+
+    LidarVolume();
+
+    //Read and store the mins and maxes from the header, calculate and store the
+    //i, j,k values and the extents
+    void CalculateBoundingBox(PULSEreader *pReader);
+};
 
 
 #endif /* LIDARVOLUME_HPP_ */

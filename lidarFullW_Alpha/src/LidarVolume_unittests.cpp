5d9f6dcb7fd67a03e76da7b7f27553ff554a4b98
rvishnkr <ravishankar@u.boisestate.edu>
2017-11-08 14:54:26 -0700

    moved pulse reading from setup to a standalone test

--- lidarFullW_Alpha/src/LidarVolume_unittests.cpp
+++ lidarFullW_Alpha/src/LidarVolume_unittests.cpp
@@ -8,52 +8,33 @@
 #include "LidarVolume.hpp"
 #include "pulsereader.hpp"
 #include "pulsewriter.hpp"
-#include <iostream>
-
 
 class LidarVolumeTest: public testing::Test{
-  public: 
-    LidarVolume ld;
 
   protected:
 
-
   // function to set up space used by all tests
   virtual void SetUp(){
-    std::string fileName =  "etc/140823_183115_1_clipped_test.pls";
-    PULSEreadOpener pOpener;  // Create a pulse read opener object
-    PULSEreader *pReader;     // Create a pulse reader object
-    pOpener.set_file_name(fileName.c_str());
-    pReader = pOpener.open();
-
-    if(pReader->read_waves){
-      CalculateBoundingBox(pReader);
-    }
+    
   }
 
 };
 
 /*
-* Given the true values of the the min and max fields describing the 
-* boundary box(x, y & z), compare it with values that are read in. 
+* Test x,y & z mins and maxes, i, j, k mins and maxes and x,y and z extents
 */ 
-TEST_F(LidarVolumeTest, xyzMinsAndMaxes){
-  //TODO:
-}
+TEST_F(LidarVolumeTest, allBoundingBoxTests){
+  LidarVolume ld;
 
-/*
-* Given the true values of the the min and max fields describing the 
-* boundary box(i, j & k), compare it with values that are read in. 
-*/
-TEST_F(LidarVolumeTest, ijkMinsAndMaxes){
-  //TODO:
-}
+  //Replace the filename with the pls file you are using for the unit test
+  std::string fileName =  "etc/140823_183115_1_clipped_test.pls";
+  
+  PULSEreadOpener pOpener;  // Create a pulse read opener object
+  PULSEreader *pReader;     // Create a pulse reader object
+  pOpener.set_file_name(fileName.c_str());
+  pReader = pOpener.open();
 
-/*
-* Given the true values of the the min and max fields describing the 
-* boundary box, compare it with values that are read in. 
-*/
-TEST_F(LidarVolumeTest, xyzExtents){
   //TODO:
-}
 
+
+}

6e49b4ebead6c4c3154fe968d8649b12a5853870
Nicholas Chapa <nchapa@r2.boisestate.edu>
2017-11-08 18:00:52 -0700

    added 2 more tests

--- lidarFullW_Alpha/src/GPSInformation_unittests.cpp
+++ lidarFullW_Alpha/src/GPSInformation_unittests.cpp
@@ -404,7 +404,7 @@ TEST_F(GPSInformationTest, checkXYZ_realMaximumValues){
 
  }
 
-TEST_F(GPSInformationTest, CheckBoundingBox){
+TEST_F(GPSInformationTest, CheckBoundingBox1){
     //This test will compare the calculated bounding box values
     //for x, y, and z with the known values
 
@@ -420,13 +420,96 @@ TEST_F(GPSInformationTest, CheckBoundingBox){
     pOpener.set_file_name(fileNameCorrect.c_str());
     pReader = pOpener.open();
 
-    int known_x_min= (int) (floor(510706.487) - 10)
-    int known_y_min= (int) (floor(4792924.648) - 10)
-    int known_z_min= (int) (floor(1604.828) - 10)
-    int known_x_max= (int) (ceiling(512412.097) + 10)
-    int known_y_max= (int) (ceiling(4794125.777) + 10)
-    int known_z_max= (int) (ceiling(1927.448) + 10)
+    int known_x_min= (int) (floor(516210) - 10);
+    int known_y_min= (int) (floor(4767920) - 10);
+    int known_z_min= (int) (floor(2084.59) - 10);
+    int known_x_max= (int) (ceil(0) + 10);
+    int known_y_max= (int) (ceil(0) + 10);
+    int known_z_max= (int) (ceil(0) + 10);
+   
+    int pulse_x_min= (int) (floor(pReader->header.min_x) - 10);
+    int pulse_y_min= (int) (floor(pReader->header.min_y) - 10);
+    int pulse_z_min= (int) (floor(pReader->header.min_z) - 10);
+    int pulse_x_max= (int) (floor(pReader->header.max_x) + 10);
+    int pulse_y_max= (int) (floor(pReader->header.max_y) + 10);
+    int pulse_z_max= (int) (floor(pReader->header.max_z) + 10);
+
+    int known_x_extent= known_x_max - known_x_min;
+    int known_y_extent= known_y_max - known_y_min;
+    int known_z_extent= known_z_max - known_z_min;
+
+    int pulse_x_extent= pulse_x_max - pulse_x_min;
+    int pulse_y_extent= pulse_y_max - pulse_y_min;
+    int pulse_z_extent= pulse_z_max - pulse_z_min;
     
+    ASSERT_EQ(known_x_min,pulse_x_min);
+    ASSERT_EQ(known_y_min,pulse_y_min);
+    ASSERT_EQ(known_z_min,pulse_z_min);
+    ASSERT_EQ(known_x_max,pulse_x_max);
+    ASSERT_EQ(known_y_max,pulse_x_max);
+    ASSERT_EQ(known_x_max,pulse_x_max);
+    
+    ASSERT_EQ(known_x_extent,pulse_x_extent);
+    ASSERT_EQ(known_x_extent,pulse_x_extent);
+    ASSERT_EQ(known_x_extent,pulse_x_extent);
+    
+
+    //pulseheader.hpp is insided fullalpha/deps/wavething/inc
+ 
+}
+
+TEST_F(GPSInformationTest, CheckBoundingBox1){
+    //This test will compare the calculated bounding box values
+    //for x, y, and z with the known values
+
+    std::string fileNameCorrect =  "etc/140823_183115_1_clipped_test.pls";
+
+    /*
+     * This section reads the wave and GPS file
+     */
+    GPSInformation gpsInfo;   // Create a GPS object
+
+    PULSEreadOpener pOpener;  // Create a pulse read opener object
+    PULSEreader *pReader;     // Create a pulse reader object
+    pOpener.set_file_name(fileNameCorrect.c_str());
+    pReader = pOpener.open();
+
+    int known_x_min= (int) (floor(510706.487) - 10);
+    int known_y_min= (int) (floor(4792924.648) - 10);
+    int known_z_min= (int) (floor(1604.828) - 10);
+    int known_x_max= (int) (ceil(512412.097) + 10);
+    int known_y_max= (int) (ceil(4794125.777) + 10);
+    int known_z_max= (int) (ceil(1927.448) + 10);
+   
+    int pulse_x_min= (int) (floor(pReader->header.min_x) - 10);
+    int pulse_y_min= (int) (floor(pReader->header.min_y) - 10);
+    int pulse_z_min= (int) (floor(pReader->header.min_z) - 10);
+    int pulse_x_max= (int) (floor(pReader->header.max_x) + 10);
+    int pulse_y_max= (int) (floor(pReader->header.max_y) + 10);
+    int pulse_z_max= (int) (floor(pReader->header.max_z) + 10);
+
+    int known_x_extent= known_x_max - known_x_min;
+    int known_y_extent= known_y_max - known_y_min;
+    int known_z_extent= known_z_max - known_z_min;
+
+    int pulse_x_extent= pulse_x_max - pulse_x_min;
+    int pulse_y_extent= pulse_y_max - pulse_y_min;
+    int pulse_z_extent= pulse_z_max - pulse_z_min;
+    
+    ASSERT_EQ(known_x_min,pulse_x_min);
+    ASSERT_EQ(known_y_min,pulse_y_min);
+    ASSERT_EQ(known_z_min,pulse_z_min);
+    ASSERT_EQ(known_x_max,pulse_x_max);
+    ASSERT_EQ(known_y_max,pulse_x_max);
+    ASSERT_EQ(known_x_max,pulse_x_max);
+    
+    ASSERT_EQ(known_x_extent,pulse_x_extent);
+    ASSERT_EQ(known_x_extent,pulse_x_extent);
+    ASSERT_EQ(known_x_extent,pulse_x_extent);
+    
+
+    //pulseheader.hpp is insided fullalpha/deps/wavething/inc
+ 
 }
 
 

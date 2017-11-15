bb659db1befebf7353d00e77f6cf38beaebd2cd2
Nick <chapa.nicholas@gmail.com>
2017-11-14 21:23:51 -0700

    renamed GPS files

---
+++ lidarFullW_Alpha/src/WaveGPSInformation.hpp
@@ -0,0 +1,41 @@
+/*
+ * File name: GPSInformation.hpp
+ * Created on: 20-June-2017
+ * Author: ravi
+ */
+
+#ifndef GPSINFORMATION_HPP_
+#define GPSINFORMATION_HPP_
+
+#include <iostream>
+#include "pulsereader.hpp"
+#include "pulsewriter.hpp"
+
+class GPSInformation{
+
+private:
+public:  
+  //possible parameters
+  double gpsTime;
+  double xAnchor, yAnchor, zAnchor;
+  double xTarget, yTarget, zTarget;
+  double xDeviation, yDeviation, zDeviation;
+  double xFirst, yFirst, zFirst;
+  double xLast, yLast, zLast;
+  unsigned char edge;
+  unsigned char facet;
+  unsigned char scanDirection;
+  unsigned char intensity;
+  
+  struct xyzCoordinates {
+    double xCoordinate;
+    double yCoordinate;
+    double zCoordinate;
+  };
+
+  GPSInformation();  
+  void populateGPS(PULSEreader *pReader);
+  void displayGPSData();
+};
+
+#endif /* GPSINFORMATION_HPP_ */

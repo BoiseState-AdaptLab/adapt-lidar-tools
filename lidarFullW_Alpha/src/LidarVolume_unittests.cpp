ef12774a1159fae1391278db0a3e7643cdc5a87e
Nick <chapa.nicholas@gmail.com>
2017-11-15 19:03:38 -0700

    Changes re-added after re-cloning the repo

--- lidarFullW_Alpha/src/LidarVolume_unittests.cpp
+++ lidarFullW_Alpha/src/LidarVolume_unittests.cpp
@@ -42,58 +42,49 @@ TEST_F(LidarVolumeTest, allBoundingBoxTests){
 
 }
 
-/*
-TEST_F(LidarVolumeTest, CheckBoundingBox1){
+
+TEST_F(LidarVolumeTest, CheckBoundingBoxClippedTest){
     //This test will compare the calculated bounding box values
-    //for x, y, and z with the known values
+    //for x, y, and z with the known values in the clipped test .pls file
 
     std::string fileNameCorrect =  "etc/140823_183115_1_clipped_test.pls";
 
     
-    GPSInformation gpsInfo;   // Create a GPS object
+    WaveGPSInformation gpsInfo;   // Create a GPS object
 
     PULSEreadOpener pOpener;  // Create a pulse read opener object
     PULSEreader *pReader;     // Create a pulse reader object
     pOpener.set_file_name(fileNameCorrect.c_str());
     pReader = pOpener.open();
+    
+    lidarVolume.calculateBoundingBox(pReader);
 
-    int known_x_min= (int) (floor(516210) - 10);
-    int known_y_min= (int) (floor(4767920) - 10);
-    int known_z_min= (int) (floor(2084.59) - 10);
-    int known_x_max= (int) (ceil(0) + 10);
-    int known_y_max= (int) (ceil(0) + 10);
-    int known_z_max= (int) (ceil(0) + 10);
-   
-    int pulse_x_min= (int) (floor(pReader->header.min_x) - 10);
-    int pulse_y_min= (int) (floor(pReader->header.min_y) - 10);
-    int pulse_z_min= (int) (floor(pReader->header.min_z) - 10);
-    int pulse_x_max= (int) (floor(pReader->header.max_x) + 10);
-    int pulse_y_max= (int) (floor(pReader->header.max_y) + 10);
-    int pulse_z_max= (int) (floor(pReader->header.max_z) + 10);
+    int known_x_min= (int) (floor(516209.586) - 10);
+    int known_y_min= (int) (floor(4767921.375) - 10);
+    int known_z_min= (int) (floor(2084.585) - 10);
+    int known_x_max= (int) (ceil(516211.942) + 10);
+    int known_y_max= (int) (ceil(4767923.621) + 10);
+    int known_z_max= (int) (ceil(2093.581) + 10);
 
     int known_x_extent= known_x_max - known_x_min;
     int known_y_extent= known_y_max - known_y_min;
     int known_z_extent= known_z_max - known_z_min;
-
-    int pulse_x_extent= pulse_x_max - pulse_x_min;
-    int pulse_y_extent= pulse_y_max - pulse_y_min;
-    int pulse_z_extent= pulse_z_max - pulse_z_min;
     
-    ASSERT_EQ(known_x_min,pulse_x_min);
-    ASSERT_EQ(known_y_min,pulse_y_min);
-    ASSERT_EQ(known_z_min,pulse_z_min);
-    ASSERT_EQ(known_x_max,pulse_x_max);
-    ASSERT_EQ(known_y_max,pulse_x_max);
-    ASSERT_EQ(known_x_max,pulse_x_max);
+    ASSERT_EQ(known_x_min,lidarVolume.pulse_bb_i_min);
+    ASSERT_EQ(known_y_min,lidarVolume.pulse_bb_j_min);
+    ASSERT_EQ(known_z_min,lidarVolume.pulse_bb_k_min);
+    ASSERT_EQ(known_x_max,lidarVolume.pulse_bb_i_max);
+    ASSERT_EQ(known_y_max,lidarVolume.pulse_bb_j_max);
+    ASSERT_EQ(known_x_max,lidarVolume.pulse_bb_k_max);
     
-    ASSERT_EQ(known_x_extent,pulse_x_extent);
-    ASSERT_EQ(known_x_extent,pulse_x_extent);
-    ASSERT_EQ(known_x_extent,pulse_x_extent);
+    ASSERT_EQ(known_x_extent,lidarVolume.pulse_x_extant);
+    ASSERT_EQ(known_x_extent,lidarVolume.pulse_y_extant);
+    ASSERT_EQ(known_x_extent,lidarVolume.pulse_z_extant);
     
 
     //pulseheader.hpp is insided fullalpha/deps/wavething/inc
  
-} */
+}
 
 TEST_F(LidarVolumeTest, CheckBoundingBox2Nayani){
     //This test will compare the calculated bounding box values

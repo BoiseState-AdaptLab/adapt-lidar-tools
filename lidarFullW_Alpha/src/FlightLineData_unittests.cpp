ecc326bc05562a61fc657aec89154f14753e9322
rvishnkr <ravishankar@u.boisestate.edu>
2017-11-20 11:50:09 -0700

    split into multiple tests

--- lidarFullW_Alpha/src/FlightLineData_unittests.cpp
+++ lidarFullW_Alpha/src/FlightLineData_unittests.cpp
@@ -19,15 +19,63 @@ class FlightLineDataTest: public testing::Test{
 
 };
 
-/*
-* Given the true values of the actual anchor, target and deviation,
+
+/****************************************************************************
+* 
+* Calling the setFlightLineData() function test
+* 
+****************************************************************************/
+TEST_F(FlightLineDataTest, testFunctionStart){
+
+  std::string incorrect_file_name1 = "bin/140823_183115_1_clipped_test.pls";
+  ASSERT_ANY_THROW (fld.setFlightLineData(incorrect_file_name1));
+
+  std::string incorrect_file_name2 = "etc/140823_183115_1_clipped_test.dat";
+  ASSERT_ANY_THROW (fld.setFlightLineData(incorrect_file_name2));
+
+  std::string correct_file_name =  "etc/140823_183115_1_clipped_test.pls";
+  ASSERT_NO_THROW (fld.setFlightLineData(correct_file_name));  
+}
+
+
+/****************************************************************************
+* 
+* Test the boundary mins and maxes
+* 
+****************************************************************************/
+TEST_F(FlightLineDataTest, testBoundaryMinsAndMaxes){
+
+  std::string file_name =  "etc/140823_183115_1_clipped_test.pls";
+  ASSERT_NO_THROW (fld.setFlightLineData(file_name));  
+
+  int known_bb_x_min = 516209;
+  int known_bb_y_min = 4767921;
+  int known_bb_z_min = 2084;
+  int known_bb_x_max = 516211;
+  int known_bb_y_max = 4767923;
+  int known_bb_z_max = 2093;
+
+  ASSERT_EQ (known_bb_x_min, fld.bb_x_min);
+  ASSERT_EQ (known_bb_y_min, fld.bb_y_min);
+  ASSERT_EQ (known_bb_z_min, fld.bb_z_min);
+  ASSERT_EQ (known_bb_x_max, fld.bb_x_max);
+  ASSERT_EQ (known_bb_y_max, fld.bb_y_max);
+  ASSERT_EQ (known_bb_z_max, fld.bb_z_max);
+
+
+}
+
+
+/****************************************************************************
+* 
+* Given the known values of the actual anchor, target and deviation,
 * we calculate the actual anchor, actual target and actual deviation
 * using FullWaveformIngestion functions
-*/
-TEST_F(FlightLineDataTest, testHeaderValues){
-
-  // Known FlightLineData 
-  int known_scanner_id = 1; 
+* 
+****************************************************************************/
+TEST_F(FlightLineDataTest, testInstrumentInformation){
+  // Known FlightLineData
+  int known_scanner_id = 1;
   double known_wave_length = 1064;
   double known_outgoing_pulse_width = 4.822761;
   int known_scan_pattern = 2;
@@ -40,27 +88,20 @@ TEST_F(FlightLineDataTest, testHeaderValues){
   double known_beam_divergence = 0.5;
   double known_minimal_range = 660.48053;
   double known_maximal_range = 1860.605225;
-  
-  std::string fileName = "bin/140823_183115_1_clipped_test.pls";
-  ASSERT_ANY_THROW (fld.setFlightLineData(fileName));
 
-  std::string fileName1 = "etc/140823_183115_1_clipped_test.dat";
-  ASSERT_ANY_THROW (fld.setFlightLineData(fileName1));
+  std::string file_name =  "etc/140823_183115_1_clipped_test.pls";
+  ASSERT_NO_THROW (fld.setFlightLineData(file_name));  
 
-  std::string fileNameCorrect =  "etc/140823_183115_1_clipped_test.pls";
-  ASSERT_NO_THROW (fld.setFlightLineData(fileNameCorrect));
- 
   ASSERT_EQ (known_scanner_id, fld.scanner_id);
   ASSERT_DOUBLE_EQ (known_wave_length, fld.wave_length);
-  ASSERT_NEAR (known_outgoing_pulse_width, fld.outgoing_pulse_width,
-              0.0000005);
+  ASSERT_NEAR (known_outgoing_pulse_width, fld.outgoing_pulse_width, 0.0000005);
   ASSERT_EQ (known_scan_pattern, fld.scan_pattern);
   ASSERT_EQ (known_numer_of_mirror_facets, fld.number_of_mirror_facets);
   ASSERT_NEAR (known_scan_frequency, fld.scan_frequency, 0.000001);
   ASSERT_NEAR (known_scan_angle_min, fld.scan_angle_min, 0.0000001);
   ASSERT_NEAR (known_scan_angle_max, fld.scan_angle_max, 0.0000005);
   ASSERT_DOUBLE_EQ (known_pulse_frequency, fld.pulse_frequency);
-  ASSERT_DOUBLE_EQ (known_beam_diameter_at_exit_aperture, 
+  ASSERT_DOUBLE_EQ (known_beam_diameter_at_exit_aperture,
                    fld.beam_diameter_at_exit_aperture);
   ASSERT_NEAR (known_beam_divergence, fld.beam_divergence, 0.01);
   ASSERT_NEAR (known_minimal_range, fld.minimal_range, 0.00001);

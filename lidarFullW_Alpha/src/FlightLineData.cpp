b1675e5676a281f3637b0b497ddf92bc4ec1fc1e
rvishnkr <ravishankar@u.boisestate.edu>
2017-11-20 09:27:37 -0700

    rempved pulse_ and modified the csv writer function

--- lidarFullW_Alpha/src/FlightLineData.cpp
+++ lidarFullW_Alpha/src/FlightLineData.cpp
@@ -10,12 +10,12 @@
 //Default constructor
 FlightLineData::FlightLineData(){
   // enter default values
-  pulse_bb_x_min = 0;
-  pulse_bb_y_min = 0;
-  pulse_bb_z_min = 0;
-  pulse_bb_x_max = 0;
-  pulse_bb_y_max = 0;
-  pulse_bb_z_max = 0;
+  bb_x_min = 0;
+  bb_y_min = 0;
+  bb_z_min = 0;
+  bb_x_max = 0;
+  bb_y_max = 0;
+  bb_z_max = 0;
   scanner_id  = 0; 
   wave_length = 0;
   outgoing_pulse_width  = 0;
@@ -43,12 +43,12 @@ void FlightLineData::setFlightLineData(std::string fileName){
     throw "Unable to Open File" + fileName;
   }
   //bounding box x,y & z mins and maxes
-  pulse_bb_x_min = pReader->header.min_x;
-  pulse_bb_y_min = pReader->header.min_y;
-  pulse_bb_z_min = pReader->header.min_z;
-  pulse_bb_x_max = pReader->header.max_x;
-  pulse_bb_y_max = pReader->header.max_y;
-  pulse_bb_z_max = pReader->header.max_z;
+  x_min = pReader->header.min_x;
+  y_min = pReader->header.min_y;
+  z_min = pReader->header.min_z;
+  x_max = pReader->header.max_x;
+  y_max = pReader->header.max_y;
+  z_max = pReader->header.max_z;
     
   int i = 1;
   while(pReader->header.get_scanner(&scanner, i)) {
@@ -91,26 +91,26 @@ void FlightLineData::FlightLineDataToCSV(){
   int i = 1;
   while(pReader->header.get_scanner(&scanner, i)) {
 
-    scannerId = i; 
-    waveLength = scanner.wave_length;
-    outgoingPulseWidth = scanner.outgoing_pulse_width;
-    scanPattern = scanner.scan_pattern;
-    numberOfMirrorFacets = scanner.number_of_mirror_facets;
-    scanFrequency = scanner.scan_frequency;
-    scanAngleMin = scanner.scan_angle_min;
-    scanAngleMax = scanner.scan_angle_max;
-    pulseFrequency = scanner.pulse_frequency;
-    beamDiameterAtExitAperture = scanner.beam_diameter_at_exit_aperture;
-    beamDivergence = scanner.beam_divergence;
-    minimalRange = scanner.minimal_range;
-    maximalRange = scanner.maximal_range;
+    scanner_id = i; 
+    wave_length = scanner.wave_length;
+    outgoing_pulse_width = scanner.outgoing_pulse_width;
+    scan_pattern = scanner.scan_pattern;
+    number_of_mirror_facets = scanner.number_of_mirror_facets;
+    scan_frequency = scanner.scan_frequency;
+    scan_angle_min = scanner.scan_angle_min;
+    scan_angle_max = scanner.scan_angle_max;
+    pulse_frequency = scanner.pulse_frequency;
+    beam_diameter_at_exit_aperture = scanner.beam_diameter_at_exit_aperture;
+    beam_divergence = scanner.beam_divergence;
+    minimal_range = scanner.minimal_range;
+    maximal_range = scanner.maximal_range;
 
     fprintf(scanout, "%d,%lf,%lf,%d,%d,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf\n",
-            scannerId, waveLength, outgoingPulseWidth, 
-            scanPattern, numberOfMirrorFacets, scanFrequency, 
-            scanAngleMin, scanAngleMax, pulseFrequency, 
-            beamDiameterAtExitAperture, beamDivergence, 
-            minimalRange, maximalRange);
+            scanner_id, wave_length, outgoing_pulse_width, 
+            scan_pattern, number_of_mirror_facets, scan_frequency, 
+            scan_angle_min, scan_angle_max, pulse_frequency, 
+            beam_diameter_at_exit_aperture, beam_divergence, 
+            minimal_range, maximal_range);
     i++;
   }
   fclose(scanout);
--- lidarFullW_Alpha/src/FlightLineData_unittests.cpp
+++ lidarFullW_Alpha/src/FlightLineData_unittests.cpp
@@ -27,19 +27,19 @@ class FlightLineDataTest: public testing::Test{
 TEST_F(FlightLineDataTest, testHeaderValues){
 
   // Known FlightLineData 
-  int TruthScannerId = 1; 
-  double TruthWaveLength = 1064;
-  double TruthOutgoingPulseWidth = 4.822761;
-  int TruthScanPattern = 2;
-  int TruthNumberOfMirrorFacets = 4;
-  double TruthScanFrequency = 146.666672;
-  double TruthScanAngleMin = 0.00001;
-  double TruthScanAngleMax = 359.999969;
-  double TruthPulseFrequency = 400;
-  double TruthBeamDiameterAtExitAperture = 10;
-  double TruthBeamDivergence = 0.5;
-  double TruthMinimalRange = 660.48053;
-  double TruthMaximalRange = 1860.605225;
+  int KnownScannerId = 1; 
+  double KnownWaveLength = 1064;
+  double KnownOutgoingPulseWidth = 4.822761;
+  int KnownScanPattern = 2;
+  int KnownNumberOfMirrorFacets = 4;
+  double KnownScanFrequency = 146.666672;
+  double KnownScanAngleMin = 0.00001;
+  double KnownScanAngleMax = 359.999969;
+  double KnownPulseFrequency = 400;
+  double KnownBeamDiameterAtExitAperture = 10;
+  double KnownBeamDivergence = 0.5;
+  double KnownMinimalRange = 660.48053;
+  double KnownMaximalRange = 1860.605225;
   
   std::string fileName = "bin/140823_183115_1_clipped_test.pls";
   ASSERT_ANY_THROW (fld.setFlightLineData(fileName));
@@ -50,19 +50,19 @@ TEST_F(FlightLineDataTest, testHeaderValues){
   std::string fileNameCorrect =  "etc/140823_183115_1_clipped_test.pls";
   ASSERT_NO_THROW (fld.setFlightLineData(fileNameCorrect));
  
-  ASSERT_EQ (truth_scanner_id, fld.scanner_id);
-  ASSERT_DOUBLE_EQ (TruthWaveLength, fld.wave_length);
-  ASSERT_NEAR (TruthOutgoingPulseWidth, fld.outgoing_pulse_width,
+  ASSERT_EQ (Known_scanner_id, fld.scanner_id);
+  ASSERT_DOUBLE_EQ (KnownWaveLength, fld.wave_length);
+  ASSERT_NEAR (KnownOutgoingPulseWidth, fld.outgoing_pulse_width,
               0.0000005);
-  ASSERT_EQ (TruthScanPattern, fld.scan_pattern);
-  ASSERT_EQ (TruthNumberOfMirrorFacets, fld.number_of_mirror_facets);
-  ASSERT_NEAR (TruthScanFrequency, fld.scan_frequency, 0.000001);
-  ASSERT_NEAR (TruthScanAngleMin, fld.scan_angle_min, 0.0000001);
-  ASSERT_NEAR (TruthScanAngleMax, fld.scan_angle_max, 0.0000005);
-  ASSERT_DOUBLE_EQ (TruthPulseFrequency, fld.pulse_frequency);
-  ASSERT_DOUBLE_EQ (TruthBeamDiameterAtExitAperture, 
+  ASSERT_EQ (KnownScanPattern, fld.scan_pattern);
+  ASSERT_EQ (KnownNumberOfMirrorFacets, fld.number_of_mirror_facets);
+  ASSERT_NEAR (KnownScanFrequency, fld.scan_frequency, 0.000001);
+  ASSERT_NEAR (KnownScanAngleMin, fld.scan_angle_min, 0.0000001);
+  ASSERT_NEAR (KnownScanAngleMax, fld.scan_angle_max, 0.0000005);
+  ASSERT_DOUBLE_EQ (KnownPulseFrequency, fld.pulse_frequency);
+  ASSERT_DOUBLE_EQ (KnownBeamDiameterAtExitAperture, 
                    fld.beam_diameter_at_exit_aperture);
-  ASSERT_NEAR (TruthBeamDivergence, fld.beam_divergence, 0.01);
-  ASSERT_NEAR (TruthMinimalRange, fld.minimal_range, 0.00001);
-  ASSERT_NEAR (TruthMaximalRange, fld.maximal_range, 0.000001);
+  ASSERT_NEAR (KnownBeamDivergence, fld.beam_divergence, 0.01);
+  ASSERT_NEAR (KnownMinimalRange, fld.minimal_range, 0.00001);
+  ASSERT_NEAR (KnownMaximalRange, fld.maximal_range, 0.000001);
 }

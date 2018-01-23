c2e0224019cd7e4241a5dd6d92f1657103c501e9
rvishnkr <ravishankar@u.boisestate.edu>
2018-01-23 14:29:42 -0700

    t_frist = 0

--- lidarFullW_Alpha/src/Peak.hpp
+++ lidarFullW_Alpha/src/Peak.hpp
@@ -7,7 +7,7 @@
 #ifndef PEAK_HPP_
 #define PEAK_HPP_
 
-double amp;
+double amp;         //Amplitude
 double location;    //The time location of peak occurence
 double fwhm;        //Full width half maximum - the width of the peak
 
@@ -15,13 +15,13 @@ int peak_position;  //Given 'n' peaks, the position of this peak
 
 int peak_triggering_location; //Peak activation time - one ahead of the noise
 
-//(peak_triggering_location - t_first) * dx + x_first
+//(peak_triggering_location) * dx + x_first
 int peak_x_activation;
 
-//(peak_triggering_location - t_first) * dy + y_first
+//(peak_triggering_location) * dy + y_first
 int peak_y_activation;
 
-//(peak_triggering_location - t_first) * dz + z_first
+//(peak_triggering_location) * dz + z_first
 int peak_z_activation;
 
 #endif /* PEAK_HPP_ */

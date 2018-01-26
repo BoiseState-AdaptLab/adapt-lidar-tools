2e9b6856f230216c15188375b6c05f9cbfb1b19b
rvishnkr <ravishankar@u.boisestate.edu>
2018-01-26 14:42:41 -0700

    added half maximum time locations and triggering_amp

--- lidarFullW_Alpha/src/Peak.hpp
+++ lidarFullW_Alpha/src/Peak.hpp
@@ -7,11 +7,16 @@
 
 double amp;         //Ampitude
 double location;    //The time location of peak occurence
-double fwhm;        //Full width half maximum - the width of the peak
+double fwhm;        //Full width at half maximum - the width of the peak
+
+//the time locations of the half maximum(amp/2)
+double fwhm_t_positive;   
+double fwhm_t_negative;
 
 int position;  //Given 'n' peaks, the position of this peak
 
-int triggering_location; //Peak activation time - one ahead of the noise
+int triggering_amp;      //Peak triggering amplitude - noise level + 1
+int triggering_location; //Peak activation time - calc using triggering_amp
 
 //(triggering_location) * dx + x_first
 int x_activation;

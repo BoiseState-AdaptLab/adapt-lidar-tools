543ceb40abddad2c76a4f02ca1b8c73cd86f0238
rvishnkr <ravishankar@u.boisestate.edu>
2018-01-26 12:16:13 -0700

    renamed instance members

--- lidarFullW_Alpha/src/Peak.hpp
+++ lidarFullW_Alpha/src/Peak.hpp
@@ -1,8 +1,6 @@
-/*
- * File name: Peak.hpp
- * Created on: 15-December-2017
- * Author: olschanowsky
- */
+//File name: Peak.hpp
+//Created on: 15-December-2017
+//Author: olschanowsky
 
 #ifndef PEAK_HPP_
 #define PEAK_HPP_
@@ -11,17 +9,17 @@ double amp;         //Ampitude
 double location;    //The time location of peak occurence
 double fwhm;        //Full width half maximum - the width of the peak
 
-int peak_position;  //Given 'n' peaks, the position of this peak
+int position;  //Given 'n' peaks, the position of this peak
 
-int peak_triggering_location; //Peak activation time - one ahead of the noise
+int triggering_location; //Peak activation time - one ahead of the noise
 
-//(peak_triggering_location) * dx + x_first
-int peak_x_activation;
+//(triggering_location) * dx + x_first
+int x_activation;
 
-//(peak_triggering_location) * dy + y_first
-int peak_y_activation;
+//(triggering_location) * dy + y_first
+int y_activation;
 
 //(peak_triggering_location) * dz + z_first
-int peak_z_activation;
+int z_activation;
 
 #endif /* PEAK_HPP_ */

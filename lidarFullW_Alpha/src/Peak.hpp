1f4c7aee186e5a17b9976df58c8464d2fbfb7f13
rvishnkr <ravishankar@u.boisestate.edu>
2018-01-26 17:06:43 -0700

    made all members public

--- lidarFullW_Alpha/src/Peak.hpp
+++ lidarFullW_Alpha/src/Peak.hpp
@@ -7,30 +7,31 @@
 
 class Peak{
 
-  double amp;         //Ampitude
-  double location;    //The time location of peak occurence
-  double fwhm;        //Full width at half maximum - the width of the peak
+  public:
+    double amp;         //Ampitude
+    double location;    //The time location of peak occurence
+    double fwhm;        //Full width at half maximum - the width of the peak
 
-  //the time locations of the half maximum(amp/2)
-  double fwhm_t_positive;   
-  double fwhm_t_negative;
+    //the time locations of the half maximum(amp/2)
+    double fwhm_t_positive;   
+    double fwhm_t_negative;
 
-  int position;  //Given 'n' peaks, the position of this peak
+    int position;  //Given 'n' peaks, the position of this peak
 
-  int triggering_amp;      //Peak triggering amplitude - noise level + 1
-  int triggering_location; //Peak activation time - calc using triggering_amp
+    int triggering_amp;      //Peak triggering amplitude - noise level + 1
+    int triggering_location; //Peak activation time - calc using triggering_amp
 
-  //(triggering_location) * dx + x_first
-  int x_activation;
+    //(triggering_location) * dx + x_first
+    int x_activation;
 
-  //(triggering_location) * dy + y_first
-  int y_activation;
+    //(triggering_location) * dy + y_first
+    int y_activation;
 
-  //(peak_triggering_location) * dz + z_first
-  int z_activation;
+    //(peak_triggering_location) * dz + z_first
+    int z_activation;
 
-  //Default constructor
-  Peak();
+    //Default constructor
+    Peak();
 
 };
 

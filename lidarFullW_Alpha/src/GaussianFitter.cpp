a731fb9dd91249e8859a965f669dfcc7b42b87a7
rvishnkr <ravishankar@u.boisestate.edu>
2018-01-26 17:12:29 -0700

    changed ln to log

--- lidarFullW_Alpha/src/GaussianFitter.cpp
+++ lidarFullW_Alpha/src/GaussianFitter.cpp
@@ -285,22 +285,22 @@ int GaussianFitter::findPeaks(std::vector<Peak>* results,
     //calculate fwhm full width at half maximum
     //model function: y= a * exp( -1/2 * [ (t - b) / c ]^2 )
     // time = +/-sqrt((-2)*(c^2)*ln(y/a) +b
-    peak->fwhm_t_positive = sqrt((-2)*(c*c)*ln(y/peak->amp)) + peak->location;      
-    peak->fwhm_t_negative = (-1)*sqrt((-2)*(c*c)*ln(y/peak->amp)) + 
+    peak->fwhm_t_positive = sqrt((-2)*(c*c)*log(y/peak->amp)) + peak->location;      
+    peak->fwhm_t_negative = (-1)*sqrt((-2)*(c*c)*log(y/peak->amp)) + 
                                                   peak->location;
     printf("fwhm_t_positive: %f\nfwhm_t_negative: %f\n", 
-            fwhm_t_positive, fwhm_t_negative);
+            peak->fwhm_t_positive, peak->fwhm_t_negative);
 
     peak->fwhm = abs(fwhm_t_positive-fwhm_t_negative)
-    printf("fwhm: %f", peak->fwhm)
+    printf("fwhm: %f", peak->fwhm);
 
     //calculate triggering location
 
     peaks->triggering_amp = noise_level + 1;
     //TODO +/-? & substitute a, b, c
     peaks->triggering_location = std::min(
-                              sqrt((-2)*(c*c)*ln(y/peak->amp)) + peak->location, 
-                         (-1)*sqrt((-2)*(c*c)*ln(y/peak->amp)) + peak->location
+                              sqrt((-2)*(c*c)*log(y/peak->amp)) + peak->location, 
+                         (-1)*sqrt((-2)*(c*c)*log(y/peak->amp)) + peak->location
                                         );
 
     //add the peak to our result

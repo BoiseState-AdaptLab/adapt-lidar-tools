b1224cbeb557b397973f5c0c9381986e096d2090
rvishnkr <ravishankar@u.boisestate.edu>
2018-01-30 14:52:58 -0700

    added deviation computation in populateGPS and renamed all variables using )

--- lidarFullW_Alpha/src/WaveGPSInformation.cpp
+++ lidarFullW_Alpha/src/WaveGPSInformation.cpp
@@ -1,9 +1,6 @@
-/*
- * File name: WaveGPSInformation.cpp
- * Created on: 20-June-2017
- * Author: ravi
-  * Modified by: Nicholas Chapa
- */
+// File name: WaveGPSInformation.cpp
+// Created on: 20-June-2017
+// Author: ravi
 
 #include <iostream>
 #include "WaveGPSInformation.hpp"
@@ -11,18 +8,26 @@
 //Default constructor
 WaveGPSInformation::WaveGPSInformation(){
   // enter default values
-  xAnchor = 0;
-  yAnchor = 0;
-  zAnchor = 0;
-  xTarget = 0;
-  yTarget = 0;
-  zTarget = 0;
-  xFirst = 0;
-  yFirst = 0;
-  zFirst = 0;
-  xLast = 0;
-  yLast = 0;
-  zLast = 0;
+  x_anchor = 0;
+  y_anchor = 0;
+  z_anchor = 0;
+  
+  x_target = 0;
+  y_target = 0;
+  z_target = 0;
+  
+  dx = 0;
+  dy = 0;
+  dz = 0;
+  
+  x_first = 0;
+  y_first = 0;
+  z_first = 0;
+  
+  x_last = 0;
+  y_last = 0;
+  z_last = 0;
+  
   edge = 0;
   facet = 0;
   scanDirection = 0;
@@ -34,18 +39,18 @@ WaveGPSInformation::WaveGPSInformation(){
   xScale = 0;
   yScale = 0;
   zScale = 0;
-  xRealAnchor = 0;
-  yRealAnchor = 0;
-  zRealAnchor = 0;
-  xRealTarget = 0;
-  yRealTarget = 0;
-  zRealTarget = 0;
-  xRealFirst = 0;
-  yRealFirst = 0;
-  zRealFirst = 0;
-  xRealLast = 0;
-  yRealLast = 0;
-  zRealLast = 0;
+  xReal_anchor = 0;
+  yReal_anchor = 0;
+  zReal_anchor = 0;
+  xReal_target = 0;
+  yReal_target = 0;
+  zReal_target = 0;
+  xReal_first = 0;
+  yReal_first = 0;
+  zReal_first = 0;
+  xReal_last = 0;
+  yReal_last = 0;
+  zReal_last = 0;
   xRealMin = 0;
   yRealMin = 0;
   zRealMin = 0;
@@ -72,20 +77,24 @@ void WaveGPSInformation::populateGPS(PULSEreader *pReader){
     //zOffset = pReader->header.x_offset;
     
     pReader->pulse.compute_anchor_and_target_and_dir();
-    xAnchor = pReader->pulse.get_anchor_x();
-    yAnchor = pReader->pulse.get_anchor_y();
-    zAnchor = pReader->pulse.get_anchor_z();
-    xTarget = pReader->pulse.get_target_x();
-    yTarget = pReader->pulse.get_target_y();
-    zTarget = pReader->pulse.get_target_z();
+    x_anchor = pReader->pulse.get_anchor_x();
+    y_anchor = pReader->pulse.get_anchor_y();
+    z_anchor = pReader->pulse.get_anchor_z();
+    x_target = pReader->pulse.get_target_x();
+    y_target = pReader->pulse.get_target_y();
+    z_target = pReader->pulse.get_target_z();
+
+    dx = (x_target - x_anchor) / 1000;
+    dy = (y_target - y_anchor) / 1000;
+    dz = (z_target - z_anchor) / 1000;
     
     pReader->pulse.compute_first_and_last();
-    xFirst = pReader->pulse.get_first_x();
-    yFirst = pReader->pulse.get_first_y();
-    zFirst = pReader->pulse.get_first_z();
-    xLast = pReader->pulse.get_last_x();
-    yLast = pReader->pulse.get_last_y();
-    zLast = pReader->pulse.get_last_z();
+    x_first = pReader->pulse.get_first_x();
+    y_first = pReader->pulse.get_first_y();
+    z_first = pReader->pulse.get_first_z();
+    x_last = pReader->pulse.get_last_x();
+    y_last = pReader->pulse.get_last_y();
+    z_last = pReader->pulse.get_last_z();
 
     edge = pReader->pulse.edge_of_scan_line;
     scanDirection = pReader->pulse.scan_direction;
@@ -97,17 +106,17 @@ void WaveGPSInformation::populateGPS(PULSEreader *pReader){
  * Displays all GPS data
  */
 void WaveGPSInformation::displayGPSData(){
-  std::cout << xAnchor << std::endl;
-  std::cout << zAnchor << std::endl;
-  std::cout << xTarget << std::endl;
-  std::cout << yTarget << std::endl;
-  std::cout << zTarget << std::endl;
-  std::cout << xFirst << std::endl;
-  std::cout << yFirst << std::endl;
-  std::cout << zFirst << std::endl;
-  std::cout << xLast << std::endl;
-  std::cout << yLast << std::endl;
-  std::cout << zLast << std::endl;
+  std::cout << x_anchor << std::endl;
+  std::cout << z_anchor << std::endl;
+  std::cout << x_target << std::endl;
+  std::cout << y_target << std::endl;
+  std::cout << z_target << std::endl;
+  std::cout << x_first << std::endl;
+  std::cout << y_first << std::endl;
+  std::cout << z_first << std::endl;
+  std::cout << x_last << std::endl;
+  std::cout << y_last << std::endl;
+  std::cout << z_last << std::endl;
   std::cout << edge << std::endl;
   std::cout << facet << std::endl;
   std::cout << scanDirection << std::endl;
@@ -115,17 +124,12 @@ void WaveGPSInformation::displayGPSData(){
 }
 
 
-void WaveGPSInformation::calculateTargetDeviations() {
-    xDeviation = (xTarget - xAnchor) / 1000;
-    yDeviation = (yTarget - yAnchor) / 1000;
-    zDeviation = (zTarget - zAnchor) / 1000;
-}
 /*
 //method of calculating real values of x, y, z discussed in 11/9 meeting
 void WaveGPSInformation::calculateOurxyz() {
-    our_x = xAnchor + (firstT + t) * xDeviation;
-    our_y = yAnchor + (firstT + t) * yDeviation;
-    our_z = zAnchor + (firstT + t) * zDeviation;
+    our_x = x_anchor + (firstT + t) * xDeviation;
+    our_y = y_anchor + (firstT + t) * yDeviation;
+    our_z = z_anchor + (firstT + t) * zDeviation;
 }
 
 //method of calculating real x, y, z values as demonstrated in pdf from Nayani
@@ -136,15 +140,15 @@ void WaveGPSInformation::getRealPoint(pulseAxisPoint, axisScale, axisOffset) {
 }
 
 //method of calculating real x, y, z values as demonstrated in pdf from Nayani
-//this method will calculate the real value of xyz anchor, target, first, last, min and max values
+//this method will calculate the real value of xyz _anchor, target, first, last, min and max values
 void WaveGPSInformation::calculateRealValues() {
-    realTarget_x = (xTarget * xScale) + xOffset;
-    realTarget_y = (yTarget * yScale) + yOffset;
-    realTarget_z = (zTarget * zScale) + zOffset;
+    real_target_x = (x_target * xScale) + xOffset;
+    real_target_y = (y_target * yScale) + yOffset;
+    real_target_z = (z_target * zScale) + zOffset;
     
-    realAnchor_x = (xAnchor * xScale) + xOffset;
-    realAnchor_y = (yAnchor * yScale) + yOffset;
-    realAnchor_z = (zAnchor * zScale) + zOffset;
+    real_anchor_x = (x_anchor * xScale) + xOffset;
+    real_anchor_y = (y_anchor * yScale) + yOffset;
+    real_anchor_z = (z_anchor * zScale) + zOffset;
     
     realMin_x = (xMin * xScale) + xOffset;
     realMin_y = (yMin * yScale) + yOffset;
@@ -154,13 +158,13 @@ void WaveGPSInformation::calculateRealValues() {
     realMax_y = (yMax * yScale) + yOffset;
     realMax_z = (zMax * zScale) + zOffset;
     
-    realFirst_x = (xFirst * xScale) + xOffset;
-    realFirst_y = (yFirst * yScale) + yOffset;
-    realFirst_z = (zFirst * zScale) + zOffset;
+    real_first_x = (x_first * xScale) + xOffset;
+    real_first_y = (y_first * yScale) + yOffset;
+    real_first_z = (z_first * zScale) + zOffset;
     
-    realLast_x = (xLast * xScale) + xOffset;
-    realLast_y = (yLast * yScale) + yOffset;
-    realLast_z = (zLast * zScale) + zOffset;
+    real_last_x = (x_last * xScale) + xOffset;
+    real_last_y = (y_last * yScale) + yOffset;
+    real_last_z = (z_last * zScale) + zOffset;
 }
 
 }
--- lidarFullW_Alpha/src/WaveGPSInformation.hpp
+++ lidarFullW_Alpha/src/WaveGPSInformation.hpp
@@ -17,11 +17,11 @@ private:
 public:  
   //possible parameters
   double gpsTime;
-  double xAnchor, yAnchor, zAnchor;
-  double xTarget, yTarget, zTarget;
-  double xDeviation, yDeviation, zDeviation;
-  double xFirst, yFirst, zFirst;
-  double xLast, yLast, zLast;
+  double x_anchor, y_anchor, z_anchor;
+  double x_target, y_target, z_target;
+  double dx, dy, dz;  //deviation
+  double x_first, y_first, z_first;
+  double x_last, y_last, z_last;
   unsigned char edge;
   unsigned char facet;
   unsigned char scanDirection;
@@ -29,17 +29,16 @@ public:
   
   double xOffset, yOffset, zOffset;
   double xScale, yScale, zScale;
-  double xRealAnchor, yRealAnchor, zRealAnchor;
-  double xRealTarget, yRealTarget, zRealTarget;
-  double xRealFirst, yRealFirst, zRealFirst;
-  double xRealLast, yRealLast, zRealLast;
+  double xReal_anchor, yReal_anchor, zReal_anchor;
+  double xReal_target, yReal_target, zReal_target;
+  double xReal_first, yReal_first, zReal_first;
+  double xReal_last, yReal_last, zReal_last;
   double xRealMin, yRealMin, zRealMin;
   double xRealMax, yRealMax, zRealMax;
   
   WaveGPSInformation();  
   void populateGPS(PULSEreader *pReader);
   void displayGPSData();
-  void calculateTargetDeviations(); 
   double calculatexPosition(double t);
   double calculateyPosition(double t);
   double calculatezPosition(double t);

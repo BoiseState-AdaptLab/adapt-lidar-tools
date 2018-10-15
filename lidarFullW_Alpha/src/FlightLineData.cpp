2b95e5df24e11f00797e381d07fd7814e993fe48
Ravi Shankar <ravishankar@u.boisestate.edu>
2018-10-15 09:54:24 -0600

    string tokenizing using stringstream

--- lidarFullW_Alpha/src/FlightLineData.cpp
+++ lidarFullW_Alpha/src/FlightLineData.cpp
@@ -54,26 +54,32 @@ void FlightLineData::setFlightLineData(std::string fileName){
   bb_z_max = pReader->header.max_z;
   
   std::string geoascii_params = pReader->header.geoascii_params;
-  std::string delimiter = "\\";
-  size_t pos = 0;
+  
   std::string utm;
   std::string geog_cs;
   std::string token;
-
-  utm = strtok (str," ");
-  utm = strtok (NULL,"/");
-  geog_cs = strtok (NULL,"/");
-
-
-  // while ((pos = geoascii_params.find(delimiter)) != std::string::npos) {
-  //     token = geoascii_params.substr(0, pos);
-  //     std::cout << token << std::endl;
-  //     geoascii_params.erase(0, pos + delimiter.length());
-  // }
+    
+    // Vector of string to save tokens 
+    vector <string> tokens; 
+      
+    // stringstream class check1 
+    stringstream check1(geoascii_params); 
+      
+    string intermediate; 
+      
+    // Tokenizing w.r.t. space ' ' 
+    while(getline(check1, intermediate, ' /')){ 
+        tokens.push_back(intermediate); 
+    } 
+      
+    // Printing the token vector 
+    for(int i = 0; i < tokens.size(); i++){
+        cout << tokens[i] << '\n'; 
+    }
 
   std::cout << "header.geoascii_params " << pReader->header.geoascii_params << std::endl;
-  std::cout << "utm: " << utm << std::endl;
-  std::cout << "geog_cs: "<< geog_cs << std::endl;
+  // std::cout << "utm: " << utm << std::endl;
+  // std::cout << "geog_cs: "<< geog_cs << std::endl;
 
 
   fprintf(stderr,"TEST: min_x %lf max_y %lf\n",bb_x_min,bb_y_max);

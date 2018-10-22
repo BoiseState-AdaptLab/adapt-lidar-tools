2e1502e201540fe38f718f10aeb97c5d5f83f79c
Ravi Shankar <ravishankar@u.boisestate.edu>
2018-10-22 12:05:28 -0600

    added i, j mins, maxes and extents

--- lidarFullW_Alpha/src/GetPLSDetails.cpp
+++ lidarFullW_Alpha/src/GetPLSDetails.cpp
@@ -34,6 +34,15 @@ int main (int argc, char *argv[]) {
   * waveforms of all pulses.*/
   double  bb_x_min = pReader->header.min_x;
   double  bb_y_max = pReader->header.max_y;
+
+  double bb_i_min = 0;
+  double bb_j_min = 0;
+  double bb_i_max = (int) (ceil(bb_x_max)) - (floor(bb_x_min));
+  double bb_j_max = (int) (ceil(bb_y_max)) - (floor(bb_y_min));
+
+  /* extent of x and yas calculated from the pulse data (max - min) */
+  double i_extent = bb_i_max - bb_i_min + 1;
+  double j_extent = bb_j_max - bb_j_min + 1;
   
   std::vector <std::string> tokens; /* vector of strings to store tokens */
   std::stringstream geo_stream(geoascii_params); /* Use stringStream to parse */
@@ -48,11 +57,22 @@ int main (int argc, char *argv[]) {
   std::string utm_str = tokens[0];
   std::string geog_cs = tokens[1];
 
+  std::cout << "\n Reading all information from the header of: " <<  argv[1]
+            << std::endl;
+
   std::cout << "\nUTM: " << utm_str << std::endl;
   std::cout << "Geog_cs: "<< geog_cs << std::endl;
 
   std::cout << "\nMin x: "<< bb_x_min << std::endl;
   std::cout << "Max y: "<< bb_y_max << std::endl;
+
+  std::cout << "\nBounding box 'i' min: "<< bb_i_min << std::endl;
+  std::cout << "Bounding box 'i' max: "<< bb_i_max << std::endl;
+  std::cout << "Bounding box 'j' min: "<< bb_j_min << std::endl;
+  std::cout << "Bounding box 'j' max: "<< bb_j_max << std::endl;
+
+  std::cout << "\nBounding box 'i' extent: "<< i_extent << std::endl;
+  std::cout << "Bounding box 'i' extent: "<< j_extent << std::endl;
   std::cout << std::endl;
 
 }

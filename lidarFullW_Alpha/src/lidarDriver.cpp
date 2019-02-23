49e661db27ee1c50d9296cbc0450ab552e3a5e39
Ahmad Rezaii <ahmadrezaii@u.boisestate.edu>
2019-02-23 00:31:52 -0700

    parse, fit, produce moved from main, #25, #26, #27

--- lidarFullW_Alpha/src/lidarDriver.cpp
+++ lidarFullW_Alpha/src/lidarDriver.cpp
@@ -15,6 +15,9 @@
 #include <chrono>
 
 typedef std::chrono::high_resolution_clock Clock;
+void parse_data(FlightLineData &);
+void fit_data(FlightLineData &, LidarVolume &);
+void produce_product(FlightLineData &, LidarVolume &);
 
 CmdLine cmdLineArgs;
 
@@ -30,97 +33,19 @@ int main (int argc, char *argv[]) {
   Clock::time_point t1 = Clock::now();
 
   std::cout << "\nProcessing  " << argv[2] << std::endl;
+
   // Create a flight line data object
   FlightLineData rawData;
-  rawData.setFlightLineData(cmdLineArgs.getInputFileName());
-
   LidarVolume intermediateData;
-  intermediateData.setBoundingBox(rawData.bb_x_min,rawData.bb_x_max,
-                                  rawData.bb_y_min,rawData.bb_y_max,
-                                  rawData.bb_z_min,rawData.bb_z_max);
-  intermediateData.allocateMemory();
 
-  PulseData pd;
-  std::ostringstream stream;
-  GaussianFitter fitter;
-  std::vector<Peak> peaks;
-  
-  #ifdef DEBUG
-    std::cerr << "Start finding peaks. In " << __FILE__ << ":" << __LINE__ << std::endl;
-  #endif
+  //parse the data
+  parse_data(rawData);
 
+  //fit data
+  fit_data(rawData,intermediateData);
 
-    std::string fit_type = cmdLineArgs.useGaussianFitting? "gaussian fitting" : "first difference";
-    std::cerr << "Finding peaks with " << fit_type << std::endl;
-
-
-
-  while(rawData.hasNextPulse()){
-    // make sure that we have an empty vector
-    for(int i=0; i<(int)peaks.size(); i++){
-      peaks.pop_back();
-    }
-    // gets the raw data from the file
-    rawData.getNextPulse(&pd);
-    try{
-      // as long as the pulse has a returning wave it finds
-      // the peaks in that wave
-      if(!pd.returningIdx.empty()){
-
-        // FOR TESTING PURPOSES
-        // for(i=0; i<(int)pd.returningWave.size(); i++){
-        //   std::cout << pd.returningWave[i] << " " ;
-        // }
-
-        // Smooth the data and test result
-        fitter.smoothing_expt(&pd.returningWave);
-
-        // Check parameter for using gaussian fitting or estimating
-	int peak_count;
-	if(cmdLineArgs.useGaussianFitting == false){
-          peak_count = fitter.guess_peaks(&peaks, pd.returningWave,
-                                          pd.returningIdx);
-	} else {
-          peak_count = fitter.find_peaks(&peaks, pd.returningWave,
-                                         pd.returningIdx);
-	}
-
-        // foreach peak - find activation point
-        //              - calculate x,y,z
-        peak_count = rawData.calc_xyz_activation(&peaks);
-
-        // give each peak to lidarVolume
-        for(int i=0;i<peak_count;i++){
-          intermediateData.insert_peak(&peaks[i]);
-        }
-      }
-    } catch (const char* msg){
-      std::cerr << msg << std::endl;
-    }
-
-    // FOR TESTING PURPOSES
-    #ifdef DEBUG
-      pd.displayPulseData(&stream);
-      std::cout << stream.str() << std::endl;
-      stream.str("");
-    #endif
-
-  }
-  
-  #ifdef DEBUG
-    std::cerr << "Total: " << fitter.get_total() << std::endl;
-    std::cerr << "Pass: " << fitter.get_pass() << std::endl;
-    std::cerr << "Fail: " << fitter.get_fail() << std::endl;
-  #endif
-
-  #ifdef DEBUG
-    std::cerr << "Peak finding complete. Going to start writing GeoTIF. In lidarDriver:94" << std::endl; 
-  #endif
-  
-  // Save the image to a geotiff file
-  // The 'title' string is stored as part of the file
-  std::cout << "Writing GeoTIFF " << std::endl;
-  intermediateData.toTif(cmdLineArgs.get_output_filename(), rawData.geog_cs, rawData.utm);
+  //produce the product
+  produce_product(rawData,intermediateData);
   
   //Get end time
   //Compute total run time and convert to appropriate units
@@ -131,4 +56,95 @@ int main (int argc, char *argv[]) {
   return 0;
 }
 
+void parse_data(FlightLineData &data){
+    data.setFlightLineData(cmdLineArgs.getInputFileName());
+}
 
+void fit_data(FlightLineData &raw_data, LidarVolume &fitted_data) {
+    fitted_data.setBoundingBox(raw_data.bb_x_min, raw_data.bb_x_max,
+                               raw_data.bb_y_min, raw_data.bb_y_max,
+                               raw_data.bb_z_min, raw_data.bb_z_max);
+    fitted_data.allocateMemory();
+
+    PulseData pd;
+    std::ostringstream stream;
+    GaussianFitter fitter;
+    std::vector<Peak> peaks;
+
+    #ifdef DEBUG
+        std::cerr << "Start finding peaks. In " << __FILE__ << ":" << __LINE__ << std::endl;
+    #endif
+
+    std::string fit_type = cmdLineArgs.useGaussianFitting ? "gaussian fitting" : "first difference";
+    std::cerr << "Finding peaks with " << fit_type << std::endl;
+
+    while (raw_data.hasNextPulse()) {
+        // make sure that we have an empty vector
+        for (int i = 0; i < (int) peaks.size(); i++) {
+            peaks.pop_back();
+        }
+        // gets the raw data from the file
+        raw_data.getNextPulse(&pd);
+        try {
+            // as long as the pulse has a returning wave it finds
+            // the peaks in that wave
+            if (!pd.returningIdx.empty()) {
+
+                // FOR TESTING PURPOSES
+                // for(i=0; i<(int)pd.returningWave.size(); i++){
+                //   std::cout << pd.returningWave[i] << " " ;
+                // }
+
+                // Smooth the data and test result
+                fitter.smoothing_expt(&pd.returningWave);
+
+                // Check parameter for using gaussian fitting or estimating
+                int peak_count;
+                if (cmdLineArgs.useGaussianFitting == false) {
+                    peak_count = fitter.guess_peaks(&peaks, pd.returningWave,
+                                                    pd.returningIdx);
+                } else {
+                    peak_count = fitter.find_peaks(&peaks, pd.returningWave,
+                                                   pd.returningIdx);
+                }
+
+                // foreach peak - find activation point
+                //              - calculate x,y,z
+                peak_count = raw_data.calc_xyz_activation(&peaks);
+
+                // give each peak to lidarVolume
+                for (int i = 0; i < peak_count; i++) {
+                    fitted_data.insert_peak(&peaks[i]);
+                }
+            }
+        } catch (const char *msg) {
+            std::cerr << msg << std::endl;
+        }
+
+        // FOR TESTING PURPOSES
+    #ifdef DEBUG
+            pd.displayPulseData(&stream);
+          std::cout << stream.str() << std::endl;
+          stream.str("");
+    #endif
+
+    }
+
+    #ifdef DEBUG
+        std::cerr << "Total: " << fitter.get_total() << std::endl;
+        std::cerr << "Pass: " << fitter.get_pass() << std::endl;
+        std::cerr << "Fail: " << fitter.get_fail() << std::endl;
+    #endif
+
+}
+
+void produce_product(FlightLineData &raw_data, LidarVolume &fitted_data){
+    #ifdef DEBUG
+        std::cerr << "Peak finding complete. Going to start writing GeoTIF. In lidarDriver:94" << std::endl;
+    #endif
+
+    // Save the image to a geotiff file
+    // The 'title' string is stored as part of the file
+    std::cout << "Writing GeoTIFF " << std::endl;
+    fitted_data.toTif(cmdLineArgs.get_output_filename(), raw_data.geog_cs, raw_data.utm);
+}

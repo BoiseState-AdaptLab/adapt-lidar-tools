387f0a76006f64445ee202e6c6aa5f22c075460c
Aaron Orenstein <aaronorenstein@u.boisestate.edu>
2019-02-28 01:24:40 -0700

    Restructured tif analysis programs. Everything is done through tiftools.py now

---
+++ lidarFullW_Alpha/scripts/tif.py
@@ -0,0 +1,185 @@
+#File name: tif.py
+#Created on: 17-December-2018
+#Author: Aaron Orenstein
+
+from osgeo import gdal
+from PIL import Image
+import numpy as np
+import math
+import sys
+import struct
+import os
+gdal.UseExceptions();
+
+class Tif:
+  def __init__(self, file_name):
+    self.file_name_full = file_name
+
+    #Get and check file name
+    last_slash = file_name.rfind("/") + 1;
+    #Cut down string to just filename.tif
+    file_name = file_name[last_slash:]
+    #Make sure file is of type .tif
+    if not file_name.endswith(".tif"):
+      print ("Invalid file: {}\nFile types supported: .tif".format(
+             file_name))
+      return
+    
+    #Initialize
+    self.file_name = file_name
+    self.getData()
+
+  #Gets average values at a y-value, y = -1 gets overall average
+  def getAvg(self, y):
+    num_data = []
+    if y >= 0:
+      num_data = [self.data[y, x] for x in range(self.maxX + 1) 
+                  if self.data[y, x] != self.no_value]
+    else:
+      num_data = [z for z in self.data.flatten() if z != self.no_value]
+    return self.no_value if len(num_data) == 0 else (sum(num_data) / len(num_data))
+
+  #Get tif data
+  def getData(self):
+    print ("\n\33[32mProcessing:\33[0m {}\n".format(self.file_name))
+
+    #Obtain raster
+    raster = gdal.Open(self.file_name_full)
+    if raster is None:
+      print ('Unable to open %s' % self.file_name_full);
+      return
+
+    #Get raster band
+    try:
+      band = raster.GetRasterBand(1)
+    except (RuntimeError, e):
+      print ('No band %i found' % 1);
+      print (e);
+      return
+
+    #Get no data value default -99999
+    self.no_value = band.GetNoDataValue()
+    if self.no_value is None:
+      self.no_value = -99999
+  
+    #Get the data from the band and return a 2D list
+    print ("Getting Data\n")
+    
+    #Data will be stored here, y = rows, x = colums
+    data = np.full((band.YSize, band.XSize), self.no_value, dtype=float)
+    #Go through each y value
+    for y in range(band.YSize):
+      #Read the raster
+      scanline = band.ReadRaster(xoff=0, yoff=y,
+                                 xsize=band.XSize, ysize=1,
+                                 buf_xsize=band.XSize, buf_ysize=1,
+                                 buf_type=gdal.GDT_Float32)
+      #Get data for all x values
+      tuple_of_floats = struct.unpack('f' * band.XSize, scanline)
+      #Go through every x value
+      for x, val in enumerate(tuple_of_floats):
+        #Make sure the data is not the no data value
+        if not (val > self.no_value - 1 and val < self.no_value + 1):
+          #Add data to data list, inputting as [row, col]
+          data[y, x] = val
+    
+    #Get max y and max x
+    self.maxX, self.maxY = band.XSize - 1, band.YSize - 1
+    #Save data
+    self.data = data
+ 
+  #Create image color data
+  def getImgData(self):
+    #Create the image
+    print ("Creating Image\n")
+    #Width is the number of columns
+    #Height is the number of rows
+    w, h = self.data.shape[1], self.data.shape[0]
+    #Min/max values of the data array
+    data_values = [i for i in self.data.flatten() if i != self.no_value]
+    max_val, min_val = max(data_values), min(data_values)
+    #Create an array that store RGB values for each data point
+    #h = number of rows, w = number of collums
+    color_data = np.full((h, w, 3), 255, dtype=np.uint8)
+    #These are the colors for each coloring 'tier'
+    #(low) dark green, yellow, red (high)
+    colors = [[0,128,0],[255,255,0],[255,0,0]]
+    num_colors = len(colors) - 1
+
+    #Loop through each y value for each x value
+    for y, vals in enumerate(self.data):
+      for x, val in enumerate(vals):
+        #Check if value is no data
+        if val != self.no_value:
+          #Normalize value between 0 and 1
+          val_frac = (val - min_val) / (max_val - min_val)
+          #Set color tiers
+          tier1, tier2 = 0, 0
+          #This is the distance between value and lower tier
+          between_frac = 0
+          if val_frac >= 0:
+            #If the value is 0 or lower, keep at lowest tier
+            if val_frac >= 1:
+              #If the value is 1 or higher, set to highest tier
+              tier1, tier2 = num_colors, num_colors
+            else:
+              #Find low tier and high tier
+              tier1 = math.floor(val_frac * num_colors)
+              tier2 = tier1 + 1
+              #Find distance between the value and the low tier (0-1)
+            between_frac = (val_frac * num_colors) - tier1
+          #Create color
+          red = (colors[tier2][0] - colors[tier1][0]) * between_frac + colors[tier1][0]
+          green = (colors[tier2][1] - colors[tier1][1]) * between_frac + colors[tier1][1]
+          blue = (colors[tier2][2] - colors[tier1][2]) * between_frac + colors[tier1][2]
+          #write color value to array, inputted as [row, col]
+          color_data[y, x] = [red, green, blue]
+    return color_data
+
+  def getComparisonImgData(self, tif2):
+    if tif2.maxY != self.maxY:
+      print ("[ Error ] Tif files are not comparable as they are not of the same length")
+      return None
+
+    #Get tif2 details
+    data2 = tif2.data 
+    #Get width and height
+    w, h = max(data2.shape[1], self.data.shape[1]), self.data.shape[0]
+    #Create color array, h is num of rows, w is num of colums
+    color_data = np.zeros((h, w, 3), dtype=np.uint8)
+    
+    #Cycle through data
+    for y, (vals1, vals2) in enumerate(zip(self.data, data2)):
+      #Make rows have equal length
+      while len(vals1) < len(vals2):
+        vals1.append(self.no_value)
+      while len(vals1) > len(vals2):
+        vals2.appen(tif2.no_value)
+      for x, (val1, val2) in enumerate(zip(vals1, vals2)):
+        no_value1 = val1 == self.no_value
+        no_value2 = val2 == tif2.no_value
+        #If nether has data, color is med gray
+        if no_value1 and no_value2:
+          color_data[y, x] = [128,128,128]
+        #If only A has data, color is blue
+        elif not no_value1 and no_value2:
+          color_data[y, x] = [0,0,255]
+        #If only B has data, color is green
+        elif no_value1 and not no_value2:
+          color_data[y, x] = [0,255,0] 
+        #Else analyze the data
+        else:
+          #Get difference and average and dif/avg
+          dif = abs(val1 - val2)
+          avg = (val1 + val2) / 2
+          #Get dif/avg up to 1, this is our color fraction
+          frac = min(1, dif / avg)
+          #Get color val out of frac
+          #First 255 goes from white to main color
+          #Second 255 goes from main color to black
+          support_color = 255 * (1 - min(1, frac * 2))
+          main_color = 255 * (1 - max(0, (frac - .5) * 2)) 
+          #write color value to array, inputted as [row, col]
+          color_data[y, x] = [main_color, support_color, support_color]
+
+    return color_data
---
+++ lidarFullW_Alpha/scripts/tiftools.py
@@ -0,0 +1,280 @@
+#File name: analyzetif.py
+#Created on: 17-December-2018
+#Author: Aaron Orenstein
+
+from osgeo import gdal
+from PIL import Image
+import numpy as np
+import math
+import sys
+import struct
+import os
+import time
+import datetime
+from pathlib import Path
+from tif import Tif
+gdal.UseExceptions()
+
+def main(arg_set, path):
+  print (arg_set)
+  global args
+  #Check if user called a comparison or not
+  if 'c' not in arg_set[0]:
+    #Create Tif object and check if we are writing an ouput
+    #and/or creating an image
+    tif = Tif(arg_set[1])
+    if 'w' in arg_set[0]:
+      writeData(tif, path)
+    if 'i' in arg_set[0]:
+      writeImage(tif, path)
+    #If arg_set not in args, then it is part of a comparison so return tif
+    if arg_set not in args:
+      return tif
+  else:
+    #Create tif objects to compare
+    tifA = main([arg_set[1], arg_set[2]], path)
+    tifB = main([arg_set[3], arg_set[4]], path)
+    #Find unused comparison number
+    j = 1
+    while (Path(path + "compare_{}_simple.out".format(j)).is_file() or
+           Path(path + "compare_{}_raw.out".format(j)).is_file() or
+           Path(path + "compare_{}.jpg".format(j)).is_file()):
+      j += 1
+    #Check if we are writing an output and/or creating an image
+    if 'w' in arg_set[0]:
+      compareData(tifA, tifB, path, j)
+    if 'i' in arg_set[0]:
+      writeCompareImg(tifA, tifB, path, j)
+  
+  #Get rid of this arg_set and move on to the next arg_set
+  args.pop(0)
+  if (len(args) > 0):
+    main(args[0], path)
+
+def writeData(tif, path):
+  #Go through each data point in the band
+  #Print data to file
+  print ("\nWriting data to file")
+  #Get data
+  data = tif.data
+  #Create output file
+  output = open(path + tif.file_name[:-4] + ".out", 'w')
+  output.write("Max Y = {}\n\n".format(tif.maxY))
+  
+  #Go through each y value
+  for y, vals in enumerate(data):
+    output.write("y = {}: ".format(y))
+    for x, val in enumerate(vals):
+      #Check if it's the last x value
+      last = x == len(vals) - 1
+      #Make sure the data is not the no data value
+      if val != tif.no_value:
+        #Write data to output file and add it do the data list
+        output.write(str(val) + ("" if last else ", "))
+      else:
+        #Write 'NA' for no data and indicate it in data list
+        output.write("NA" + ("" if last else ", "))
+    output.write("\n\n") 
+  output.close()
+
+#Writes image data to a jpg
+def writeImage(tif, path):
+  #Get color data
+  color_data = tif.getImgData()
+  img = Image.fromarray(color_data, 'RGB')
+  img.save(path + tif.file_name[:-4] + '.jpg')
+  print("Image written to {}\n".format(path + tif.file_name[:-4] + '.jpg'))
+
+def compareData(tifA, tifB, path, compare_no):
+  A = 0
+  B = 1
+  #Get max Y's from the first line of the file
+  if not tifA.maxY == tifB.maxY:
+    print("Tif data files are not of equal length.\n" +
+          "File A has a max y of {}. File B has a max y of {}."
+	  .format(tifA.maxY, tifB.maxY))
+    return
+  
+  #Create file to write to
+  #'Simple' has the totals for each comparison result
+  #'Raw' has the comparison for every data point
+  outputs = [open(path + "compare_{}_simple.out".format(compare_no), 'w'),
+             open(path + "compare_{}_raw.out".format(compare_no), 'w')]
+  #Write the date, time, and input file names on the top of the output files
+  for out in outputs:
+    out.write("Created at {}\n".format(datetime.datetime.fromtimestamp(time.time()).
+              strftime('%Y-%m-%d %H:%M:%S')))
+    out.write("file A is {}, file B is {}\n".format(tifA.file_name, tifB.file_name))
+    out.write("Max Y = {}\n".format(tifA.maxY))
+  outputs[0].write("The simple file contains the totals for each comparison result for each y-value\n\n")
+  outputs[1].write("The raw file contains the comparison result for each data point\n\n")
+
+  #Begin comparison
+  print ("\33[32mBeginning comparison\33[0m\n")
+  #Cycle through the data for each y value
+  for y in range(tifA.maxY + 1):
+    #Write the current y value to the 'raw' file
+    outputs[0].write("y = {}: ".format(y))
+    outputs[1].write("y = {}: ".format(y))
+    #0 = both no data, 1 = A has data, 2 = B has data, 3 = both have data
+    comparison = [0,0,0,0]
+    #This holds the differences in values
+    differences = []
+    first = True
+
+    #Compare each data point and write the result to the 'raw' file
+    for x in range(min([tifA.maxX, tifB.maxX])):
+      #Get data points and no values
+      a, b = tifA.data[y,x], tifB.data[y,x]
+      nvA, nvB = tifA.no_value, tifB.no_value
+      #', ' is added before the next data comparison
+      #This way is will not be added after the last data point
+      #However we don't want it before the first data point
+      if not first:
+        outputs[1].write(", ")
+      if a != nvA and b != nvB:
+        #Both
+        outputs[1].write("[{}|{}]".format(a, b))
+        comparison[3] += 1
+        differences.append(float(a) - float(b))
+      elif a != nvA and b == nvB:
+        #Just A
+        outputs[1].write("[{}|NA]".format(a))
+        comparison[1] += 1
+      elif a == nvA and b != nvB:
+        #Just B
+        outputs[1].write("[NA|{}]".format(b))
+        comparison[2] += 1
+      else:
+        #Neither
+        outputs[1].write("NA")
+        comparison[0] += 1
+      first = False
+
+    outputs[1].write("\n\n")
+    #Get averages of data and differences
+    a_avg, b_avg = tifA.getAvg(y), tifB.getAvg(y)
+    a_avg = "n/a" if a_avg == tifA.no_value else a_avg
+    b_avg = "n/a" if b_avg == tifB.no_value else b_avg
+    dif_avg = "n/a" if len(differences) == 0 else sum(differences)/len(differences)
+    #Write analysis into file
+    outputs[0].write(
+      "total data points: {}\nneither: {}\nonly A: {}\nonly B: {}\nboth: {}\n".format(
+      sum(comparison), comparison[0], comparison[1], comparison[2], comparison[3])
+      + "average A: {}\naverage B: {}\naverage difference (A - B): {}\n\n".format(
+      a_avg, b_avg, dif_avg))
+  print ("Comparison complete\nResults have been written to the compare_{}.out files\n"
+         .format(compare_no))
+
+def writeCompareImg(tif1, tif2, path, compare_no):
+  #Get color data
+  color_data = tif1.getComparisonImgData(tif2)
+  im = Image.fromarray(color_data, 'RGB')
+  im.save("{}compare_{}.jpg".format(path, compare_no))
+  print ("Comparison complete\nImage has been written to compare_{}.jpg\n".format(compare_no))
+
+def isTif(file_name):
+  return file_name[file_name.rfind("/") + 1:].endswith(".tif")
+
+if __name__ == '__main__':
+  usage = """
+    Usage:
+    $ python {} -iw input-raster1 -iwc [-iw] input-raster1 [-iw] input-raster2
+      [-p path-name]
+        input-raster: input tif file
+        -i: creates an image from the tif file or comparison
+        -w: writes data for tif file or comparison to a .out file
+        -c: compares two input files
+            use -i and -w normally for each input-raster
+    	-p path-name: Output file destination from your current location
+    """.format(sys.argv[0])
+
+  #Check arguments
+  i = 1
+  #Stores custom path
+  custom_path = ""
+  #Store any relevent arguments
+  args = []
+  #Store previous arg
+  prev_arg = ""
+  
+  while i < len(sys.argv):
+    arg = sys.argv[i]
+    if arg == "-p":
+      #Make sure next argument is a valid path
+      i += 1
+      if len(sys.argv) == i or not os.path.isdir(sys.argv[i]):
+        print ("""
+    [ Error ] invalid or missing path
+
+    {}""".format(usage))
+        sys.exit(1)
+      else:
+        custom_path = sys.argv[i]
+        if custom_path[-1:] != "/":
+          custom_path += "/"
+    #If argument is not a file
+    elif '.' not in arg:
+      #Check for unfilled space in the last arg
+      if len(args) > 0 and '' in args[-1]:
+        #If the working arg has -c and current arg has -c
+        #Then -c does not have its two input files
+        if 'c' in args[-1][0] and 'c' in arg:
+          print ("[ Error ] 'c' needs two input files\nSkipping")
+          args.pop()
+        #If the working arg has -i or -w and current arg is some -
+        #The -i/-w does not have its input file
+        elif 'c' not in prev_arg and '.' not in prev_arg:
+          print ("[ Error ] 'w' and 'i' need an input file\nSkipping")
+          args.pop()
+      #Check if user is calling a comparison (-c)
+      if 'c' in arg:
+        args.append([arg,'','','',''])
+      #Check if -w or -i are being added to a comparison or not
+      elif len(args) == 0 or 'c' not in args[-1][0]:
+        args.append([arg,''])
+      else:
+        args[-1][args[-1].index('')] = arg
+    #If argument is a file
+    else:
+      #Check if it is a tif
+      if not isTif(arg):
+        print ("[ Error ] file must be of type tif\nSkipping")
+      #Check for previous argument
+      elif len(args) == 0 or '' not in args[-1]:
+        print ("[ Error ] {} requires an argument before it\nSkipping".format(arg))
+      #Use input file
+      else:
+        #Not part of a comparison
+        if 'c' not in args[-1][0]:
+          args[-1][1] = arg
+        else:
+          #Find placement index
+          idx = args[-1].index('')
+          #If index is even, it is the file index and already has an arg before
+          if idx % 2 == 0:
+            args[-1][idx] = arg
+          #Otherwise we need to add an arg
+          else:
+            args[-1][idx] = "-"
+            args[-1][idx + 1] = arg
+    prev_arg = arg
+    i += 1
+
+  #Check if last argument had enough inputs
+  if len(args) > 0 and '' in args[-1]:
+    print ("[ Error ] {}\nSkipping".format("'c' needs two input files" if
+      'c' in args[-1][0] else "'w' and 'i' need an input file"))
+    args.pop()
+  
+  #Check if we have any arguments
+  if len(args) == 0:
+    print ("""
+    [ Error ] you must supply at least 1 argument:
+    1) -iw input_raster
+    OR
+    1) -iwc [-iw] input_raster1 [-iw] input_raster2
+    {}""".format(usage))
+    sys.exit(1)
+
+main(args[0], custom_path);

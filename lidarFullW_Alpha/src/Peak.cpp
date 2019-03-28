4f8bafee52182bb5a5c98b81aeafb2be87cf42a2
SpencerFleming <spencerfleming@u.boisestate.edu>
2019-03-28 16:07:48 -0600

    Correts naming conventions for #57 and #60

--- lidarFullW_Alpha/src/Peak.cpp
+++ lidarFullW_Alpha/src/Peak.cpp
@@ -1,4 +1,4 @@
-//File name: Peak.hpp
+//File name: Peak.cpp
 //Created on: 26-January-2018
 //Author: ravi
 
@@ -6,25 +6,23 @@
 
 //Default constructor
 Peak::Peak(){
-  amp = 0;        
-  location = 0;   
-  fwhm = 0;       
+  amp = 0;
+  location = 0;
+  fwhm = 0;
 
-  //positionInWave //No default value currently
-  is_final_peak = false;
-
-  fwhm_t_positive = 0;   
+  fwhm_t_positive = 0;
   fwhm_t_negative = 0;
 
-  position = 0; 
+  is_final_peak = false;
+  position_in_wave = 0;
 
-  triggering_amp = 0;     
+  triggering_amp = 0;
   triggering_location = 0;
- 
+
   x_activation = 0;
   y_activation = 0;
   z_activation = 0;
-  
+
   x = 0.;
   y = 0.;
   z = 0.;

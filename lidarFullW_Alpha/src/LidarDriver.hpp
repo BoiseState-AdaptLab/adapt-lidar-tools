3603b49699180d110662d666c2366dca09dde549
Catherine Olschanowsky <catherineolschan@boisestate.edu>
2019-04-19 15:28:28 -0600

    Fixing formatting during code review

--- lidarFullW_Alpha/src/LidarDriver.hpp
+++ lidarFullW_Alpha/src/LidarDriver.hpp
@@ -21,22 +21,28 @@ const double MAX_ELEV = 99999.99;
 
 class LidarDriver {
 public:
-	void setup_flight_data(FlightLineData &data, std::string inputFileName);
-	void fit_data(FlightLineData &, LidarVolume &, bool useGaussianFitting);
-	void produce_product(LidarVolume &, GDALDataset *, int );
-	void setup_lidar_volume(FlightLineData &, LidarVolume &);
-	int parse_pulse(PulseData &, std::vector<Peak> &, GaussianFitter &, bool , int &);
-	void add_peaks_to_volume(LidarVolume &,std::vector<Peak> &, int );
-	GDALDriver * setup_gdal_driver();
-	GDALDataset * setup_gdal_ds(GDALDriver *tiff_driver, std::string filename, std::string band_desc,
-			int x_idx_extent, int y_idx_extent);
-	void geo_orient_gdal(LidarVolume &, GDALDataset *, std::string , int );
-	float get_extreme_diff(std::vector<Peak> *peaks, char peak_property);
-	float get_extreme(std::vector<Peak> *peaks, bool max_flag, int peak_pos, char peak_property);
-	float get_mean(std::vector<Peak> *peaks, int peak_pos, char peak_property);
-	double get_deviation(std::vector<Peak> *peaks, double avg, int peak_pos, char peak_property);
-	float get_peak_property(Peak *peak, char peak_property);
-	double get_skewtosis(std::vector<Peak> *peaks, double avg, double dev, int peak_pos, char peak_property, int power);
+
+  void setup_flight_data(FlightLineData &data, std::string inputFileName);
+  void fit_data(FlightLineData &, LidarVolume &, bool useGaussianFitting);
+  void produce_product(LidarVolume &, GDALDataset *, int );
+  void setup_lidar_volume(FlightLineData &, LidarVolume &);
+  int parse_pulse(PulseData &, std::vector<Peak> &, GaussianFitter &,
+                  bool , int &);
+  void add_peaks_to_volume(LidarVolume &,std::vector<Peak> &, int );
+  GDALDriver * setup_gdal_driver();
+  GDALDataset * setup_gdal_ds(GDALDriver *tiff_driver,
+                              std::string filename, std::string band_desc,
+                              int x_idx_extent, int y_idx_extent);
+  void geo_orient_gdal(LidarVolume &, GDALDataset *, std::string , int );
+  float get_extreme_diff(std::vector<Peak> *peaks, char peak_property);
+  float get_extreme(std::vector<Peak> *peaks, bool max_flag, int peak_pos,
+                    char peak_property);
+  float get_mean(std::vector<Peak> *peaks, int peak_pos, char peak_property);
+  double get_deviation(std::vector<Peak> *peaks, double avg, int peak_pos,
+                       char peak_property);
+  float get_peak_property(Peak *peak, char peak_property);
+  double get_skewtosis(std::vector<Peak> *peaks, double avg, double dev,
+                       int peak_pos, char peak_property, int power);
 };
 
 
--- lidarFullW_Alpha/src/PlsToGeotiff.cpp
+++ lidarFullW_Alpha/src/PlsToGeotiff.cpp
@@ -11,20 +11,22 @@ typedef std::chrono::high_resolution_clock Clock;
 // Lidar driver
 int main (int argc, char *argv[]) {
 
-	LidarDriver driver; //driver object with tools
-    CmdLine cmdLineArgs; //command line options
-	FlightLineData rawData; //the raw data read from PLS + WVS files
-	LidarVolume intermediateData; //the parsed data from the LIDAR waveforms
+  LidarDriver driver; //driver object with tools
+  CmdLine cmdLineArgs; //command line options
+  FlightLineData rawData; //the raw data read from PLS + WVS files
+  LidarVolume intermediateData; //the parsed data from the LIDAR waveforms
+
 
   // Parse and validate the command line args
-    if(!cmdLineArgs.parse_args(argc,argv)){
-        exit(1);
-    }
+  if(!cmdLineArgs.parse_args(argc,argv)){
+    return 1;
+  }
 
   //Collect start time
   Clock::time_point t1 = Clock::now();
 
-  std::cout << "\nProcessing  " << cmdLineArgs.getInputFileName().c_str() << std::endl;
+  std::cout << "\nProcessing  " << cmdLineArgs.getInputFileName().c_str() 
+            << std::endl;
 
   //ingest the raw flight data into an object
   driver.setup_flight_data(rawData, cmdLineArgs.getInputFileName());
@@ -35,34 +37,38 @@ int main (int argc, char *argv[]) {
   //Represents the output file format. This is used only to write data sets
   GDALDriver *driverTiff = driver.setup_gdal_driver();
 
+  // TODO: None of this should be in main - it should be abstracted away
   //produce the product(s)
   for(const int& prod : cmdLineArgs.selected_products){
-	  std::cout << "Writing GeoTIFF "<< cmdLineArgs.get_product_desc(prod) << std::endl;
-	  //represents the tiff file
-	  GDALDataset *gdal_ds;
-	  //Setup gdal dataset for this product
-	  gdal_ds = driver.setup_gdal_ds(driverTiff, cmdLineArgs.get_output_filename(prod).c_str(),cmdLineArgs
-			  .get_product_desc(prod), intermediateData.x_idx_extent,intermediateData.y_idx_extent);
+    std::cout << "Writing GeoTIFF "<< cmdLineArgs.get_product_desc(prod) 
+              << std::endl;
+    //represents the tiff file
+    GDALDataset *gdal_ds;
+    //Setup gdal dataset for this product
+    gdal_ds = driver.setup_gdal_ds(driverTiff, 
+                                 cmdLineArgs.get_output_filename(prod).c_str(),
+                                 cmdLineArgs.get_product_desc(prod),
+                                 intermediateData.x_idx_extent,
+                                 intermediateData.y_idx_extent);
 
-	  //orient the tiff correctly
-	  driver.geo_orient_gdal(intermediateData,gdal_ds, rawData.geog_cs, rawData.utm);
+    //orient the tiff correctly
+    driver.geo_orient_gdal(intermediateData,gdal_ds,
+                          rawData.geog_cs, rawData.utm);
 
-	  //write the tiff data
-	  driver.produce_product(intermediateData, gdal_ds, prod);
+    //write the tiff data
+    driver.produce_product(intermediateData, gdal_ds, prod);
 
-	  //kill it with fire!
-	  GDALClose((GDALDatasetH) gdal_ds);
+    //kill it with fire!
+    GDALClose((GDALDatasetH) gdal_ds);
   }
-
   GDALDestroyDriverManager();
-
   intermediateData.deallocateMemory();
 
   //Get end time
   Clock::time_point t2 = Clock::now();
 
   //Compute total run time and convert to appropriate units
-  double diff = std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count();
+  double diff = std::chrono::duration_cast<std::chrono::seconds>(t2-t1).count();
   std::cout << "All done!\nTime elapsed: " << diff << " seconds\n" << std::endl;
 
   return 0;

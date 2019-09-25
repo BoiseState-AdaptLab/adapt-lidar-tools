4c5208cb9f76410ec884d8e83d84ad4db72c3328
Ariel Marvasti <amarvasti@r2.boisestate.edu>
2019-09-24 18:03:20 -0600

    #326 added script to run gprof on geotiff-driver

--- scripts/run_gprof.sh
+++ scripts/run_gprof.sh
@@ -1,4 +1,5 @@
 make clean
-sh install.sh -d
+make PROFILER_BUILD=1 geotiff-driver
 bin/geotiff-driver -f etc/Nayani_clipped_test.pls -a 4,8,17
 gprof bin/geotiff-driver gmon.out > analysis.txt
+mv *.tif gmon.out analysis.txt bin

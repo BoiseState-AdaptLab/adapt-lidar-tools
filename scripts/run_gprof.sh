725a2bcbd8a6576795b46cabd5f276542a625965
Ariel Marvasti <amarvasti@r2.boisestate.edu>
2019-10-21 10:42:36 -0600

    #351 Changed run_gprof.sh script to run with slurm with cmd:sbatch run_gprof.sh

--- scripts/run_gprof.sh
+++ scripts/run_gprof.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=1:00:00   # walltime
+#SBATCH --ntasks=1   # number of processes (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH -p adaptlab   # partition(s)
+#SBATCH --mem-per-cpu=1G   # memory per CPU core
+#SBATCH -J "lidarProfiling"   # job name
+#SBATCH --mail-user=CHANGE_ME   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+
+module load gsl/gcc/2.4
+module load gdal/intel/2.2.2
 make clean
 make PROFILER_BUILD=1 geotiff-driver
-bin/geotiff-driver -f etc/Nayani_clipped_test.pls -a 4,8,17
-gprof bin/geotiff-driver gmon.out > analysis.txt
-mv *.tif gmon.out analysis.txt bin
+bin/geotiff-driver -f etc/Nayani_clipped_test.pls -w 13,11,14 -a 4,8,17 -e 1,2,3
+gprof bin/geotiff-driver | scripts/gprof2dot/gprof2dot.py | dot -Tpng -o prof.png
+mv *.tif gmon.out bin
